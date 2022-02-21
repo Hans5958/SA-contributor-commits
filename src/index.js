09b35ae4f6d5bf1872ccfe9a5e50b564e8178644
Hans5958 <hans5958@outlook.com>
2022-02-21 13:40:14 +0700

    Code from website-v2 for simplicity

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const fetchJson = async url => {
  * Respond to the request
  * @param {Request} request
  */
-const handleRequest = async request => {
+const getCommits = async request => {
 	try {
 		const org = "ScratchAddons"
 		// const { data } = await octokit.repos.listForOrg({org})
@@ -54,14 +54,7 @@ const handleRequest = async request => {
 		}))
 		totalContributors.sort((a, b, field='login') => (a[field] > b[field]) - (a[field] < b[field]))
 		// console.log(stringifyB(totalContributors, null, '\t'))
-		fs.writeFileSync('contributors.json', stringifyB(totalContributors, null, '\t'))
-		// return new Response(JSON.stringify(totalContributors), {
-		// 	status: 200,
-		// 	headers: {
-		// 		"Content-Type": "application/json;charset=UTF-8",
-		// 		"Cache-Control": "max-age=60",
-		// 		"Access-Control-Allow-Origin": "*"
-		// 	}
+		return totalContributors
 		// })
 	} catch (e) {
 		console.error(e)
@@ -69,6 +62,51 @@ const handleRequest = async request => {
 	}
 }
 
-handleRequest()
-
 console.log(`::set-output name=TIMESTAMP::${new Date().toISOString()}`)
+
+;(async () => {
+	let contributors = []
+
+	await Promise.all([
+
+		// Fetch contributors data from ScratchAddons/contributors, with all-contributors spec
+		(() => new Promise(async callback => {
+			setTimeout(async () => {
+				let response = await (await fetch("https://raw.githubusercontent.com/ScratchAddons/contributors/master/.all-contributorsrc")).json()
+				// console.log(contributors)
+				// console.log(response)
+				response.contributors.forEach(responseItem => {
+					let index = contributors.findIndex(contributorsItem => contributorsItem.login === responseItem.login)
+					if (index === -1) {
+						contributors.push({})
+						index = contributors.length - 1
+					}
+					Object.assign(contributors[index], responseItem)
+				})
+				// console.log(contributors)
+				// console.log(response)
+				callback()
+			}, 3000);
+		}))(),
+
+		// Fetch commit count data from all repositories
+		(() => new Promise(async callback => {
+			let response = await getCommits()
+			while (contributors.length === 0) await new Promise(resolve => setTimeout(resolve, 250))
+			response.forEach(responseItem => {
+				let index = contributors.findIndex(contributorsItem => contributorsItem.login === responseItem.login)
+				if (index === -1) {
+					contributors.push({})
+					index = contributors.length - 1
+				}
+				responseItem.commits = responseItem.contributions
+				delete responseItem.contributions
+				Object.assign(contributors[index], responseItem)
+			})
+			callback()
+		}))()
+	])
+
+	fs.writeFileSync('contributors.json', stringifyB(contributors, null, '\t'))
+
+})()
